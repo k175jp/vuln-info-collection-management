568d2a2c936738749923083d93e201c69e986eb0
k175 <82032493+k175jp@users.noreply.github.com>
2023-11-20 22:53:16 +0900

    [Update] コメントの追加

--- main.py
+++ main.py
@@ -11,23 +11,28 @@ from extract.extract_data import extract_nvd
 
 
 def main():
-    cpe_list = get()
-    result = {}
-    for key, value in cpe_list.items():
+    cpe_list = get() # cpe/cpe.listをdictで取得
+    for key, value in cpe_list.items(): 
         for v in value:
             cpe = v['cpe']
             for c in cpe:
                 part, vender, product = c.split(':')
+
+                # cpeから脆弱性情報の取得してdictオブジェクトを受け取る
                 # jvn_vuln = get_jvn(part, vender, product)
                 nvd_vuln = get_nvd(part, vender, product)
+
+                # 上のデータからデータフォーマットに沿ってデータを抽出
                 # jvn_data = extract_jvn(jvn_vuln)
                 nvd_data = extract_nvd(nvd_vuln)
+
+                # 上のデータを統合
                 # data = integrate(product, jvn_data, nvd_data)
+
+                # 上のデータを保存
                 # save(key, data)
-                time.sleep(6)
-                break
-            break
-        break
+                time.sleep(6) # 30秒で5リクエスト = 6秒で1リクエスト(nvd)
+
     
 if __name__ == '__main__':
     main()
