20c6d89caf2d7dfc825f37f4e35f2f5bdb20284e
k175 <82032493+k175jp@users.noreply.github.com>
2023-11-20 22:54:46 +0900

    [Update] コメントの追加

--- vuln/get_vuln_cpe.py
+++ vuln/get_vuln_cpe.py
@@ -5,8 +5,11 @@ def get_jvn(part, vender, product):
     pass
 
 def get_nvd(part, vender, product):
+    # uriの生成
     uri = f'https://services.nvd.nist.gov/rest/json/cves/2.0?virtualMatchString=cpe:2.3:{part}:{vender}:{product}:*:*:*:*:*&noRejected'
+    # リクエストの送信
     response = requests.get(uri)
+    # jsonデータをdictに変換
     json_data = json.loads(response.text)
 
     return json_data
